1c251ecf78355ff75e471184dfef4ef7d9fd43ad
aheath <aaron.heath5447@gmail.com>
2023-10-16 19:05:57 -0400

    Add flow control for evaluating an answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 // Select Elements
 var startBtn = document.querySelector("#quiz-start");
+var questionBox = document.querySelector("#question-box");
+var questionTitle = document.querySelector("#quiz-title");
+var questionContent = document.querySelector("#question-content");
+var questionOptions;
+
+// Set Global Variables
 var questionSet = [];
 var optionSet = [];
+var questionCounter = 0;
+var score = 0;
 
-// Declare Classes Variables
+// Declare Classes
 class Question {
     constructor(text, options) {
         this.text = text;
@@ -20,8 +28,70 @@ class Option {
 
 
 // Declare Functions
+var renderQuestion = function() {
+    if(questionCounter >= questionSet.length-1) {
+        alert("quiz over!");
+        return;
+    }
+
+    let currentQuestion = questionSet[questionCounter];
+    try {
+        questionOptions.innerHTML = "";
+    } catch (e) {
+        console.log(e);
+    }
+    
+    // Title
+    questionTitle.textContent = currentQuestion.text;
+    // Create list element
+    questionOptions = document.createElement("ol");
+    
+    
+    // Create list items for each option
+    for(let option of currentQuestion.options) {
+        // Create list element
+        let listElement = document.createElement("li");
+        // Set li text
+        listElement.textContent = option.text;
+        // Set li attributes
+        listElement.setAttribute("data-validation",option.correct);
+        listElement.setAttribute("class","quiz-btn");
+        // Append LI to OL
+        questionOptions.appendChild(listElement);
+    }
+    questionContent.setAttribute("style", "text-align: left;");
+    questionBox.setAttribute("style", "align-items: start;");
+    // Append OL to Content
+    questionContent.appendChild(questionOptions);
+
+    questionCounter ++;
+
+}
+
+var evaluateAnswer = function(event) {
+    var element = event.target;
+    // Only run function if an li element was clicked
+    if(element.matches("li")) {
+        // Boolean from string value
+        let correctAnswer = element.dataset.validation === "true";
+
+
+        if(correctAnswer) {
+            alert("correct!");
+            renderQuestion();
+        } else {
+            alert("WRONG");
+        }
+
+
+    } else {
+
+        return;
+    }
+}
+
 var startQuiz = function() {
-    alert("Quiz has started!");
+    // ("Quiz has started!");
     // Build Quiz
     // Question 1
     optionSet.push(new Option("Square Brackets",false));
@@ -40,6 +110,7 @@ var startQuiz = function() {
     optionSet.push(new Option("Developer Orientation Manual", false));
     optionSet.push(new Option("Document Ordered Model", false));
     question = new Question("In Web Development, what does DOM stand for?", optionSet);
+    questionSet.push(question);
 
     optionSet=[];
 
@@ -49,6 +120,7 @@ var startQuiz = function() {
     optionSet.push(new Option("Boolean", false));
     optionSet.push(new Option("Confirm", true));
     question = new Question("Which of the following is not a primative data type in JavaScript?", optionSet);
+    questionSet.push(question);
 
     optionSet = [];
     
@@ -57,13 +129,19 @@ var startQuiz = function() {
     optionSet.push(new Option("Enumerate", false));
     optionSet.push(new Option("For loops", true));
     optionSet.push(new Option("While loops", false));
-    
-    question = new Question("Fill in the blank: In JavaScript, use _______ to iterate over the items in an array.")
+
+    question = new Question("Fill in the blank: In JavaScript, use _______ to iterate over the items in an array.", optionSet)
+    questionSet.push(question);
+
+    startBtn.remove();
+    // questionContent.remove();
+    questionContent.textContent = "";
+    renderQuestion();
 }
 
 // Add Event Listeners
 startBtn.addEventListener("click", startQuiz);
-
+questionBox.addEventListener("click",evaluateAnswer);
 
 
 
